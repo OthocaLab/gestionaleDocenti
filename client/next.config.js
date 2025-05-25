44dc684b70e57f9ffa46bc779d6d90d14b155e8b
Alessandro Porcu <alessandroporcu.it@gmail.com>
2025-05-25 16:34:46 +0200

    [FE]  Frontend allowedDevOrigins

--- client/next.config.js
+++ client/next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
     allowedDevOrigins: process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS 
       ? process.env.NEXT_PUBLIC_ALLOWED_DEV_ORIGINS.split(',').map(origin => origin.trim())
-      : ['localhost:3000'],
+      : [
+          'localhost:3000',
+          '127.0.0.1:3000',
+          'gestionaledocenti.ddns.net',
+          '3.121.230.179:3000',
+          '172.26.10.221:3000'
+        ],
     async rewrites() {
       return [
         {
